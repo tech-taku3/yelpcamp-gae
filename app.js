b5f6f74cfc9e49e75dc02c497abc3d1cfa80d670
tech-taku3 <tech.taku3@gmail.com>
2025-05-01 21:41:48 +0900

    39_YelpCamp キャンプ場の一覧

--- app.js
+++ app.js
@@ -20,11 +20,10 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({title: '私の庭', description: '気軽に安くキャンプ!!', });
-    await camp.save();
-    res.send(camp);
-})
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds});
+});
 
 app.listen(3000, () => {
     console.log('ポート3000でリクエスト待受中...');
