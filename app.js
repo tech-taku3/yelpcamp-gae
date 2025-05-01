0127324ab38e9907677bfdfd93717f2f6b24e197
tech-taku3 <tech.taku3@gmail.com>
2025-05-01 21:09:31 +0900

    39_YelpCamp キャンプ場のモデル作成

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelpCamp')
+    .then(() => {
+        console.log('MongoDBコネクションOK!!!');
+    })
+    .catch(err => {
+        console.log('MongoDBコネクションエラー!!!');
+        console.log(err);
+    });
+const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -8,6 +19,12 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     res.render('home');
 });
+
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({title: '私の庭', description: '気軽に安くキャンプ!!', });
+    await camp.save();
+    res.send(camp);
+})
 
 app.listen(3000, () => {
     console.log('ポート3000でリクエスト待受中...');
