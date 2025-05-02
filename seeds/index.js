5731d53e3f8fe536c69921fb4c2bfaee48e9e745
tech-taku3 <tech.taku3@gmail.com>
2025-05-02 14:19:43 +0900

    41_YelpCamp: 基本的なスタイルをあてる 画像を追加
    modelに`image`を追加
    seeds/index.jsにシードするランダムデータ生成の処理を追加。画像はpicsum.photosのAPIからランダムに画像を取得
    詳細ページに画像とdescription追加

--- seeds/index.js
+++ seeds/index.js
@@ -18,9 +18,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++) {
         const randomCityIndex = Math.floor(Math.random() * cities.length);
+        const price = Math.floor(Math.random() * 2000) + 1000;
         const camp = new Campground({
             location: `${cities[randomCityIndex].prefecture}${cities[randomCityIndex].city}`,
-            title: `${sample(descriptors)}・${sample(places)}`
+            title: `${sample(descriptors)}・${sample(places)}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: '吾輩は猫である。名前はまだ無い。どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。吾輩はここで始めて人間というものを見た。しかもあとで聞くとそれは書生という人間中で一番獰悪な種族であったそうだ。この書生というのは時々我々を捕えて煮て食うという話である。しかしその当時は何という考もなかったから別段恐しいとも思わなかった。ただ彼の掌に載せられてス',
+            price
         });
         await camp.save();
     };
