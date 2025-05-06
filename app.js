20ace2077c1bbbbdd074d3a2ae713cc4e5da360d
tech-taku3 <tech.taku3@gmail.com>
2025-05-07 08:39:10 +0900

    43_YelpCamp:エラーハンドリングとバリデーション 基本的なエラーハンドリング

--- app.js
+++ app.js
@@ -40,10 +40,14 @@ app.get('/campgrounds/:id', async (req, res) => {
     res.render('campgrounds/show', { campground });
 });
 
-app.post('/campgrounds', async (req, res) => {
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`)
+app.post('/campgrounds', async (req, res, next) => {
+    try{
+        const campground = new Campground(req.body.campground);
+        await campground.save();
+        res.redirect(`/campgrounds/${campground._id}`);
+    } catch (e) {
+        next(e);
+    }
 });
 
 app.get('/campgrounds/:id/edit', async (req, res) => {
@@ -61,6 +65,10 @@ app.delete('/campgrounds/:id', async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+});
+
+app.use((err, req, res, next) => {
+    res.send('問題が起きました');
 });
 
 app.listen(3000, () => {
