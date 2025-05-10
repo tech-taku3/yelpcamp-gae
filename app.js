9f19afd70ff5edd91338ff014009fc3864643b24
tech-taku3 <tech.taku3@gmail.com>
2025-05-10 10:40:51 +0900

    43_YelpCamp:エラーハンドリングとバリデーション JOIでスキーマバリデーション

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const Joi = require('joi');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
@@ -43,7 +44,22 @@ app.get('/campgrounds/:id', catchAsync(async (req, res) => {
 }));
 
 app.post('/campgrounds', catchAsync(async (req, res) => {
-    if (!req.body.campground) throw new ExpressError('不正なキャンプ場のデータです', 400);
+    // if (!req.body.campground) throw new ExpressError('不正なキャンプ場のデータです', 400);
+    const campgroundSchema = Joi.object({
+        campground: Joi.object({
+            title: Joi.string().required(),
+            price: Joi.number().required().min(0),
+            image: Joi.string().required(),
+            location: Joi.string().required(),
+            description: Joi.string().required(),
+        }).required()
+    });
+
+    const{ error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(detail => detail.message).join(',');
+        throw new ExpressError(msg, 400);
+    }
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
