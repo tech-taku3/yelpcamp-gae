6be7a0b6639215375e4f0fdd117f89fe09d8e156
tech-taku3 <tech.taku3@gmail.com>
2025-05-17 10:19:37 +0900

    46_YelpCamp:レビューモデルの追加 レビューモデルの作成

--- models/campground.js
+++ models/campground.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const { Schema } = mongoose;
 
 const campgroundSchema = new Schema({
     title: String,
     image: String,
     price: Number,
     description: String,
-    location: String
+    location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 });
 
 module.exports = mongoose.model('Campground', campgroundSchema);
