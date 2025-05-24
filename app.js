30a1e6269474d3af9e4ed879fefe0e1e74e1cff5
tech-taku3 <tech.taku3@gmail.com>
2025-05-24 13:43:18 +0900

    49_YelpCamp:リファクタとフラッシュ Reviewのルーティングを引っ越し
    親側(app.js useでルーティングを呼び出すところ）で:idなどreq.paramsを使っている場合、
    子供側(router定義しているところ)でreq.paramsを使うには、
    `const router = express.Router({ mergeParams: true });`
    を設定する必要がある。

--- app.js
+++ app.js
@@ -2,14 +2,12 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const { campgroundSchema, reviewSchema } = require('./schemas');
-const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
-const Campground = require('./models/campground');
-const Review = require('./models/review');
+
 
 const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelpCamp')
     .then(() => {
@@ -28,47 +26,12 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
-const validateCampground = (req, res, next) => {
-    const{ error } = campgroundSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(detail => detail.message).join(',');
-        throw new ExpressError(msg, 400);
-    } else {
-        next();
-    }
-}
-
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(detail => detail.message).join(',');
-        throw new ExpressError(msg, 400);
-    } else {
-        next();
-    }
-}
-
 app.get('/', (req, res) => {
     res.render('home');
 });
 
 app.use('/campgrounds', campgroundRoutes);
-
-app.post('/campgrounds/:id/reviews',validateReview, catchAsync(async(req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    await campground.save();
-    await review.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-}));
-
-app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async(req, res) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId} });
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/campgrounds/${id}`);
-}));
+app.use('/campgrounds/:id/reviews', reviewRoutes)
 
 // 講義では、('*', (req, ..))だが、5系以降挙動が変わっているらしい。Q&Aで似た事象があり、この書き方でエラーが解消された。
 app.all('{*any}', (req, res, next) => {
