3906a1546f2ab80db115a0e25d1edbcd2417d492
tech-taku3 <tech.taku3@gmail.com>
2025-05-25 16:33:17 +0900

    51_YelpCamp: 認証を追加する ユーザー登録の実装

--- models/user.js
+++ models/user.js
@@ -14,6 +14,16 @@ const userSchema = new Schema({
 // passportLocalMongooseの機能をuserSchemaに組み込む。
 // → ユーザーネーム、パスワードハッシュの情報がSchemaに組み込まれる
 // その他便利なメソッドも使えるように
-userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(passportLocalMongoose, {
+    errorMessages: {
+        UserExistsError: 'そのユーザー名はすでに使われています。',
+        MissingPasswordError: 'パスワードを入力してください。',
+        AttemptTooSoonError: 'アカウントがロックされています。時間を明けて再度試してください。',
+        TooManyAttemptsError: 'ログインの失敗が続いていたため、アカウントをロックしました。',
+        NoSaltValueStoredError: '認証ができませんでした。',
+        IncorrectPasswordError: 'パスワードまたはユーザー名が間違っています。',
+        IncorrectUsernameError: 'パスワードまたはユーザー名が間違っています。',
+    }
+});
 
 module.exports = mongoose.model('User', userSchema);
--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,18 @@ router.get('/register', (req, res) => {
 });
 
 router.post('/register', async (req, res) => {
-    res.send(req.body);
-})
+    try {
+        const { email, username, password} = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password); // passportの方で、生のpasswordをハッシュ化したうえでインスタンスを作成
+        console.log(registeredUser);
+        req.flash('success', 'Yelp Campへようこそ！');
+        res.redirect('/campgrounds');
+    } catch(e) {
+        req.flash('error', e.message);
+        res.redirect('/register');
+    }
+    
+});
 
 module.exports = router;
