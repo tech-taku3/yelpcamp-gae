dbd06a0f828978dc60fc1cc6880faa48aaf1fde5
tech-taku3 <tech.taku3@gmail.com>
2025-05-25 16:55:18 +0900

    51_YelpCamp: 認証を追加する isLoggedInミドルウェア
    middleware.jsをトップレベルに作成する
    passpostが、reqにisAuthenticated()メソッドを追加してくれており、これを用いて認証ロジックを作成する。
    
    ※ 画面のバリデーションだけでなく、post,edit,put, deleteなどを、curlなど画面以外から実行される可能性もあるため忘れず守る

---
+++ middleware.js
@@ -0,0 +1,9 @@
+module.exports.isLoggedIn = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        req.flash('error', 'ログインしてください');
+        return res.redirect('/login');
+    }
+    next();
+}
+
+
