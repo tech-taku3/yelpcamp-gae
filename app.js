855a196af7aaaf0d53755f663c3315445c73b582
tech-taku3 <tech.taku3@gmail.com>
2025-05-25 16:02:33 +0900

    51_YelpCamp: 認証を追加する ユーザー登録フォーム

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 
+const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
@@ -60,12 +61,7 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/fakeUser', async (req, res) => {
-    const user = new User({email: 'hogegege@example.com', username: 'hogegege'});
-    const newUser = await User.register(user, 'mogegege');
-    res.send(newUser);
-})
-
+app.use('/', userRoutes);
 app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/reviews', reviewRoutes)
 
