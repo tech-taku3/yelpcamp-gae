8d9b15de2da8c7d4c8f6e7fa36ff6e029421d2d9
tech-taku3 <tech.taku3@gmail.com>
2025-05-27 21:17:42 +0900

    52_YelpCamp:基本的な認可を追加 レビューの権限
    reviewsにauthor(usersとのリレーション)作成
    ログイン状態でレビューフォームの出し分け
    レビュー投稿時、author保存

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-const { validateReview } = require('../middleware');
+const { isLoggedIn, validateReview } = require('../middleware');
 const catchAsync = require('../utils/catchAsync');
 const Campground = require('../models/campground');
 const Review = require('../models/review');
 
-router.post('/',validateReview, catchAsync(async(req, res) => {
+router.post('/', isLoggedIn, validateReview, catchAsync(async(req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
     await campground.save();
     await review.save();
