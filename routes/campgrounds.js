dc3f8dd42965be856a0cda52e7a2b97eb3694d2e
tech-taku3 <tech.taku3@gmail.com>
2025-05-28 19:44:16 +0900

    53_YelpCamp: コントローラーと星を使った評価 キャンプ場のコントローラー作成

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,67 +1,21 @@
 const express = require('express');
+const campgrounds = require('../controllers/campgrounds');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
-const Campground = require('../models/campground');
 const { isLoggedIn, validateCampground, isAuthor } = require('../middleware');
 
-router.get('/', catchAsync(async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds });
-}));
+router.get('/', catchAsync(campgrounds.index));
 
-router.get('/new', isLoggedIn, (req, res) => {
-    res.render('campgrounds/new');
-});
+router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
-router.get('/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
-    .populate({
-        path: 'reviews',        // 今までのキーと同じ
-        populate: {             // さらに、reviewsのインスタンスに対してpopulateして
-            path: 'author'      // authorを
-        }
-    }).populate('author');
-    console.log(campground);
-    if (!campground) {
-        req.flash('error', 'キャンプ場は見つかりませんでした');
-        return res.redirect('/campgrounds');
-    }
-    res.render('campgrounds/show', { campground });
-}));
+router.get('/:id', catchAsync(campgrounds.showCampground));
 
-router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
-    // if (!req.body.campground) throw new ExpressError('不正なキャンプ場のデータです', 400);
-    const campground = new Campground(req.body.campground);
-    campground.author = req.user._id; // passportが用意しているreq.userのプロパティを使う
-    await campground.save();
-    req.flash('success', '新しいキャンプ場を登録しました');
-    res.redirect(`/campgrounds/${campground._id}`);
-}));
+router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
 
-router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id);
-    if (!campground) {
-        req.flash('error', 'キャンプ場は見つかりませんでした');
-        return res.redirect('/campgrounds');
-    }
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
 
-    res.render('campgrounds/edit', { campground });
-}));
+router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
 
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    
-    const camp = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-    req.flash('success', 'キャンプ場を更新しました');
-    res.redirect(`/campgrounds/${camp._id}`)
-}));
-
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    await Campground.findByIdAndDelete(id);
-    req.flash('success', 'キャンプ場を削除しました');
-    res.redirect('/campgrounds');
-}));
+router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 module.exports = router;
