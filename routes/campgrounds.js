5726a4366e5384620151ab59b22dcc8fe4fb9668
tech-taku3 <tech.taku3@gmail.com>
2025-05-30 21:25:42 +0900

    54_YelpCamp: 画像アップロード multerミドルウェア
    enctype="multiple/form-data"をフォームに指定
    input type="file"を作成
    → req.bodyにはそのままでは入らない。
    `npm i multer`
    multerを使い、upload.single('image')を実行することで、req.fileにfileのオブジェクトが入る
    destに指定したディレクトリにfileが保存される

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,10 +3,21 @@ const campgrounds = require('../controllers/campgrounds');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, validateCampground, isAuthor } = require('../middleware');
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 router.route('/')  // EXpress app.route()を使ってパスをグルーピング
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+    // imageというフィールドをfileとしてパースしてくれる
+    // .post(upload.single('image'),(req, res) => {
+    //     console.log(req.body, req.file);
+    //     res.send('受け付けました');
+    // })
+    .post(upload.array('image'),(req, res) => {
+        console.log(req.body, req.files);
+        res.send('受け付けました');
+    })
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
@@ -16,7 +27,5 @@ router.route('/:id')
     .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
-
-router
 
 module.exports = router;
