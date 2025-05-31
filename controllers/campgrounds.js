1d8636561cfd5c7221cc00db7bde95f8211a121e
tech-taku3 <tech.taku3@gmail.com>
2025-05-31 17:08:25 +0900

    54_YelpCamp: 画像アップロード フォームに画像削除を追加

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -47,6 +47,7 @@ module.exports.renderEditForm = async (req, res) => {
 }
 
 module.exports.updateCampground = async (req, res) => {
+    console.log(req.body);
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     const imgs = req.files.map(f => ({ url: f.path, filename: f.filename}));
