14acbf07bc1f71c340b4f3cca3485afeb5cab4e1
tech-taku3 <tech.taku3@gmail.com>
2025-06-04 08:54:30 +0900

    56_YelpCamp: クラスターマップ クラスターマップで遊ぶ Sourceをcampgroundsに修正

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -11,7 +11,7 @@ mapboxgl.accessToken = mapToken;
         // Add a new source from our GeoJSON data and
         // set the 'cluster' option to true. GL-JS will
         // add the point_count property to your source data.
-        map.addSource('earthquakes', {
+        map.addSource('campgrounds', {
             type: 'geojson',
             // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
             // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
@@ -26,7 +26,7 @@ mapboxgl.accessToken = mapToken;
         map.addLayer({
             id: 'clusters',
             type: 'circle',
-            source: 'earthquakes',
+            source: 'campgrounds',
             filter: ['has', 'point_count'],
             paint: {
                 // Use step expressions (https://docs.mapbox.com/style-spec/reference/expressions/#step)
@@ -58,7 +58,7 @@ mapboxgl.accessToken = mapToken;
         map.addLayer({
             id: 'cluster-count',
             type: 'symbol',
-            source: 'earthquakes',
+            source: 'campgrounds',
             filter: ['has', 'point_count'],
             layout: {
                 'text-field': ['get', 'point_count_abbreviated'],
@@ -70,7 +70,7 @@ mapboxgl.accessToken = mapToken;
         map.addLayer({
             id: 'unclustered-point',
             type: 'circle',
-            source: 'earthquakes',
+            source: 'campgrounds',
             filter: ['!', ['has', 'point_count']],
             paint: {
                 'circle-color': '#11b4da',
@@ -86,7 +86,7 @@ mapboxgl.accessToken = mapToken;
                 layers: ['clusters']
             });
             const clusterId = features[0].properties.cluster_id;
-            map.getSource('earthquakes').getClusterExpansionZoom(
+            map.getSource('campgrounds').getClusterExpansionZoom(
                 clusterId,
                 (err, zoom) => {
                     if (err) return;
